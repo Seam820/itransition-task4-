011266f7d477f09dc3d289e67320c6875ec009bc
Solaiman islam seam <contactwithseam@gmail.com>
2026-06-09 21:42:03 +0600

    Fix axios import in AdminPanel.jsx

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'ajax';
+import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 
 const AdminPanel = () => {
