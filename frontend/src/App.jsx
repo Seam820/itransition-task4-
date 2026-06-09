67220a2a55a417b00795f556ed150889c7dea07e
Solaiman islam seam <contactwithseam@gmail.com>
2026-06-09 23:13:03 +0600

    Add PrivateRoute for protected AdminPanel access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminPanel from './pages/AdminPanel';
 
+
 const PrivateRoute = ({ children }) => {
     const { token } = useAuth();
     return token ? children : <Navigate to="/login" />;
@@ -18,11 +19,21 @@ function App() {
                     <Routes>
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
+                        
+                        
+                        <Route path="/" element={
+                            <PrivateRoute>
+                                <AdminPanel />
+                            </PrivateRoute>
+                        } />
+
+                        
                         <Route path="/admin" element={
                             <PrivateRoute>
                                 <AdminPanel />
                             </PrivateRoute>
                         } />
+                    
                         <Route path="*" element={<Navigate to="/login" />} />
                     </Routes>
                 </div>
